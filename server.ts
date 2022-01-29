a84f47d61bce7495663fff6ce6f36df956bbf0ad
Chia1104 <38397958+Chia1104@users.noreply.github.com>
2022-01-29 22:27:26 +0800

    change port to 8888

--- server.ts
+++ server.ts
@@ -1,7 +1,7 @@
 import { Application } from "https://deno.land/x/oak/mod.ts";
 import router from "./routes.ts";
 
-const PORT = 3000;
+const PORT = 8888;
 const app = new Application();
 
 app.use(router.routes());
