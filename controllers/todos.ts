7ec8c17e9784089ff3b3b14b8bdfb6e8eea8d5fe
Chia1104 <yuyuchia7423@gmail.com>
2022-09-01 11:19:55 +0800

    Update dotenv mod.

--- controllers/todos.ts
+++ controllers/todos.ts
@@ -1,5 +1,5 @@
-import { config } from "https://deno.land/x/dotenv/mod.ts";
-const { DATA_API_KEY, APP_ID } = config();
+import { config } from "https://deno.land/std@0.153.0/dotenv/mod.ts";
+const { DATA_API_KEY, APP_ID } = await config();
 const BASE_URI = `https://data.mongodb-api.com/app/${APP_ID}/endpoint/data/beta/action`;
 const DATA_SOURCE = "Cluster0";
 const DATABASE = "todo_db";
